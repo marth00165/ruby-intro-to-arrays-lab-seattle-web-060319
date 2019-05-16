627d99a43dd385c6e27d792a8840ca982a6da43f
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-16 02:49:46 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  dragonBallZ = []
+  return dragonBallZ
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["Goku", "Vegeta"]
+end
+
+def first_element(kamehameha)
+  kamehameha[0]
+end
+
+def third_element(bigBang)
+  bigBang[2]
+end
+
+def last_element(finalFlash)
+  finalFlash[-1]
+end
+
+def first_element_with_array_methods(instantTransmission)
+  instantTransmission.first 
+end
+
+def last_element_with_array_methods(superSaiyen)
+  superSaiyen.last
+end
+
+def length_of_array(majinVegeta)
+  majinVegeta.length
+end
+
